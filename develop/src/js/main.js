aafef66efb5eb3b93fa28d84dd3fe38b614d93f5
user1 <doantam8924@gmail.com>
2019-05-29 18:08:23 +0900

    Finish branding pc

--- develop/src/js/main.js
+++ develop/src/js/main.js
@@ -1,27 +1,3 @@
-
-// jQuery(document).ready(function() {
-//   var url = $(location).attr('../js.main.js')
-//   var isClosed = false;
-//   var autoCloseTimer = false;
-//   setTimeout(addScrollListener, 5000);
-//   function addScrollListener() {
-//     window.addEventListener('scroll', function () {
-//       clearTimeout(autoCloseTimer);
-//         if (isClosed || window.pageYOffset > window.innerHeight * .5) {
-//           if (isClosed) {
-//             isClosed = false;
-//               $('.cont_r').animate({ opacity: 0 }, 400, function () {
-//                 $(this).css({
-//                   opacity: 1,
-//                   top: 0
-//                  });
-//               });
-//             }
-//             $('.cont_r').addClass('isOpen');
-//             autoCloseTimer = setTimeout(autoClose, 8000);
-//              else {
-//             $('.cont_r').removeClass('isOpen');
-//             }
-//       }
-//     }
-// });
+$(document).ready(function(){
+  
+});
