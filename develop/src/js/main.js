64ac71a73d73aeb54d0250c663b76442daf78a74
nho tam <>
2019-06-03 01:29:07 +0900

    update js

--- develop/src/js/main.js
+++ develop/src/js/main.js
@@ -1,3 +1,6 @@
+$(window).on('resize',function(){
+  resize();
+})
 $(function() {
   $(".menu_btn").click(function() {
     if ($("#cv_nav").hasClass("hidden")) {
@@ -8,44 +11,19 @@ $(function() {
     $(this).toggleClass("open");
   });
 
-  $("#cv_nav").click(function() {
-    if ($("#cv_nav").hasClass("visible")) {
-      $(".menu_btn").toggleClass("open");
-    } else {
-    }
-    $(this).attr("class", "slideOutLeft hidden");
-  });
-
-  $(document).keyup(function(e) {
-    if (e.keyCode == 27) {
-      if ($("#cv_nav").hasClass("visible")) {
-        $(".menu_btn").toggleClass("open");
-      } else {
-      }
-      $("#cv_nav").attr("class", "slideOutLeft hidden");
-    }
-  });
 //scroll//
 
       $(window).scroll(function(){
           var heightcont = $('.cont_r').outerHeight()
           var heightwindown = $( window ).height()
           var heightscreen = heightcont - heightwindown
-
-          console.log('hh', heightscreen)
-          console.log('sc', $(this).scrollTop())
           if($(this).scrollTop() > 50) {
             $('#header').hide();
+            $(".cont_r").show(1000);
             $('.cont_r').addClass('opencontent')
           } else {
             $('.cont_r').removeClass('opencontent')
             $('#header').show();
           }
-          if($(this).scrollTop() > heightscreen) {
-            $('.cont_r').addClass('opencontent1')
-          } else {
-            $('.cont_r').removeClass('opencontent1')
-          }
       });
-
 });
